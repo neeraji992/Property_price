3d1c9e379c8883c827c1f9e72fa925c0cd1a0740
Neeraj Thakur <119390117+neeraji992@users.noreply.github.com>
2024-12-29 18:45:20 +0530

    Delete wsgi.py

--- wsgi.py
+++
@@ -1,5 +0,0 @@
-from waitress import serve
-from app import app  # assuming your Flask app is in 'app.py'
-
-if __name__ == "__main__":
-    serve(app, host='0.0.0.0', port=8080)  # Bind to 0.0.0.0 for external access
